b29232b73f665d956be6ae0dfde8a35c13175d89
Antoine A <antoine_avenia@hotmail.fr>
2023-04-07 15:48:44 +0200

    re-organize order test

--- delete_user.php
+++ delete_user.php
@@ -1,15 +1,8 @@
 <?php
     function delete_user(string $username) {
-        if(!file_exists("../passwords/".$username.".txt")):
-            $errors = array(
-                "code" => 24,
-                "message" => "Username does not exist"
-            );
-            return $errors;
-        endif;
         if($username == ""):
             $errors = array(
-                "code" => 25,
+                "code" => 23,
                 "message" => "Please fill in all fields"
             );
             return $errors;
@@ -17,8 +10,15 @@
         $patern = "/^([A-Za-z0-9]){3,20}$/";
         if(!preg_match($patern, $username)):
             $errors = array(
-                "code" => 26,
+                "code" => 24,
                 "message" => "Username must contain only letters and numbers"
+            );
+            return $errors;
+        endif;
+        if(!file_exists("../passwords/".$username.".txt")):
+            $errors = array(
+                "code" => 25,
+                "message" => "Username does not exist"
             );
             return $errors;
         endif;
@@ -30,7 +30,7 @@
             );
         } catch (Exception $e) {
             $errors = array(
-                "code" => 27,
+                "code" => 400,
                 "message" => "An error occurred : " . $e->getMessage()
             );
             return $errors;
--- disconnect_user.php
+++ disconnect_user.php
@@ -1,15 +1,8 @@
 <?php
     function disconnect_user(string $username) {
-        if(!file_exists("../passwords/".$username.".txt")):
-            $errors = array(
-                "code" => 20,
-                "message" => "Username does not exist"
-            );
-            return $errors;
-        endif;
         if($username == ""):
             $errors = array(
-                "code" => 21,
+                "code" => 20,
                 "message" => "Please fill in all fields"
             );
             return $errors;
@@ -17,8 +10,15 @@
         $patern = "/^([A-Za-z0-9]){3,20}$/";
         if(!preg_match($patern, $username)):
             $errors = array(
-                "code" => 22,
+                "code" => 21,
                 "message" => "Username must contain only letters and numbers"
+            );
+            return $errors;
+        endif;
+        if(!file_exists("../passwords/".$username.".txt")):
+            $errors = array(
+                "code" => 22,
+                "message" => "Username does not exist"
             );
             return $errors;
         endif;
@@ -30,7 +30,7 @@
             );
         } catch (Exception $e) {
             $errors = array(
-                "code" => 23,
+                "code" => 400,
                 "message" => "An error occurred : " . $e->getMessage()
             );
             return $errors;
